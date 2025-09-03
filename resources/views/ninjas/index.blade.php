4251c92ccad81665205bdc825598083517406abc
Azkizhraa <azkizhraa@gmail.com>
2025-09-03 19:13:38 +0700

    updated

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ninja network | Home</title>
+</head>
+<body>
+    <h1>Current available ninjas</h1>
+    <ul>
+        <li>Ninjas here</li>
+    </ul>
+</body>
+</html>
